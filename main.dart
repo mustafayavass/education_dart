0b8ab5e49f2c5c5a48917c4c0e16c99170213324
YunusDemirr <mustafayavass52@gmail.com>
2024-10-22 17:27:56 +0300

    dart

---
+++ main.dart
@@ -0,0 +1,18 @@
+void main() {
+  //1.Değişken ismi rakamla başlayamaz.
+  //2.Değişken isimlerinde özel karakterler kullanılmaz(?,*,/,!)
+  //3.Değişken isimlerinde türkçe karakterler kullanılmaz.(ü,ı,ç,ö,İ,ğ)
+  //4.Değişken isimlerinde boşluk kullanılmaz.
+
+  //KOŞUL YAPILARI
+  //1.İF-ELSE
+  //2.SWİTCH-CASE
+  /*double pi = 3.14;
+  String metin = "Merhaba";*/
+  int yas = 18;
+  if (yas < 18) {
+    print("Reşit değilsiniz");
+  } else {
+    print("Reşitsiniz.");
+  }
+}
