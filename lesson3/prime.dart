1faeda1f45774148c9ef7bdb767b585aeff74b2c
YunusDemirr <mustafayavass52@gmail.com>
2024-11-05 17:25:54 +0300

    education

---
+++ lesson3/prime.dart
@@ -0,0 +1,28 @@
+//void: ana fonksiyona değer döndürmeyen
+
+
+void prime()
+{
+  List<int> prime = [];
+  bool isprime;
+  for (int i = 10; i < 100; i++) 
+  {
+    isprime = true;
+    for (int j = 2; j < i; j++) 
+    {
+      if (i % j == 0) 
+      {
+        isprime = false;
+        break;
+      }
+    }
+    if (isprime == true) 
+    {
+      prime.add(i);
+    }
+  }
+
+  for (var number in prime) {
+    print(number);
+  }
+}
